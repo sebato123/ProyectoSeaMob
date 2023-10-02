19048d7c197cca62a25e4a3c2e63a2587869d2d7
TheMob009 <143637105+TheMob009@users.noreply.github.com>
2023-10-02 19:02:02 -0300

    Cree System basico y listas

---
+++ ProyectoPOO-1/ProyectoPOO-1/SistemaUsuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPOO_1
+{
+    internal class SistemaUsuario
+    {
+    }
+}
